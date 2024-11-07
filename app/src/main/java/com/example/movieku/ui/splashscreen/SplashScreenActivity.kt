b1946cbdeab9ac1dca6712c311f7805bf064c6dd
miftahulacil0608 <miftahulacil0608@gmail.com>
2024-11-07 23:46:20 +0700

    System Bar configuration themes.xml

--- app/src/main/java/com/example/movieku/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/movieku/ui/splashscreen/SplashScreenActivity.kt
@@ -24,7 +24,6 @@ class SplashScreenActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
 
         //TODO logic in here
